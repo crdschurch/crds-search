f5bbbbc9f8bab77dbec7f7ab3d9fece35237f76c
Luke Moderwell <luke.moderwell@gmail.com>
2019-01-16 14:55:25 -0500

    add message hit

---
+++ src/app/components/message-hit/message-hit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-hit',
+  templateUrl: './message-hit.component.html',
+  styleUrls: ['./message-hit.component.scss']
+})
+export class MessageHitComponent {
+
+  constructor() { }
+
+}
