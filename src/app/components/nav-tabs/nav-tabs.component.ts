067014c002c7a9f265f43a37129f81e593b4d020
Luke Moderwell <luke.moderwell@gmail.com>
2019-01-15 21:56:50 -0500

    attempt to filter through tab navs

--- src/app/components/nav-tabs/nav-tabs.component.ts
+++ src/app/components/nav-tabs/nav-tabs.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-tabs',
   templateUrl: './nav-tabs.component.html',
   styleUrls: ['./nav-tabs.component.scss']
 })
-export class NavTabsComponent implements OnInit {
-
+export class NavTabsComponent {
+  @Input() hits;
+  @Input() results;
   constructor() { }
 
-  ngOnInit() {
+  public handleFilterClick(event: Event) {
+    var filter = event.target.innerText;
+    var refinementInput = document.querySelector(`input[value=${filter}]`);
+    if (!refinementInput.checked) {
+      refinementInput.click();  
+    }
   }
 
 }
