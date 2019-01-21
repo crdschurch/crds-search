a56261e8da1f9c604adfcf88e110f96550aea83f
Luke Moderwell <luke.moderwell@gmail.com>
2019-01-21 11:19:25 -0500

    remove unused tab nav styles

--- src/app/components/refinement-list/refinement-list.component.ts
+++ src/app/components/refinement-list/refinement-list.component.ts
@@ -1,35 +1,13 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-refinement-list',
   templateUrl: './refinement-list.component.html',
   styleUrls: ['./refinement-list.component.scss']
 })
-export class RefinementList implements OnChanges {
+export class RefinementList {
   @Input() hits;
   @Input() results;
 
-  currentRefinements = [];
-
   constructor() { }
-
-  public setFilters() {
-    var refinements: any = document.querySelectorAll('.ais-RefinementList-checkbox');
-    for (var i = 0; i < refinements.length; i += 1) {
-      if (refinements[i].value !== null) {
-        this.currentRefinements.push(refinements[i].value);
-      }
-    }
-  }
-
-  public handleFilterClick(event: any) {
-    // var filter = event.target.innerText;
-    // if (!this.refinementInput.checked) {
-    //   this.refinementInput.click();  
-    // }
-  }
-
-  ngOnChanges(changes) {
-    this.setFilters();
-  }
 }
