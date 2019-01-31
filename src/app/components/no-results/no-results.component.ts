50b2890c5c715492519a4e9a7d229153bd44fc6e
Luke Moderwell <luke.moderwell@gmail.com>
2019-01-31 10:21:42 -0500

    US16224: No Results dynamic URLs for corkboard/groups search

--- src/app/components/no-results/no-results.component.ts
+++ src/app/components/no-results/no-results.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment'
 @Component({
   selector: 'app-no-results',
   templateUrl: './no-results.component.html',
   styleUrls: ['./no-results.component.scss']
 })
 
-export class NoResultsComponent {
+export class NoResultsComponent implements OnInit {
   @Input() hits;
   @Input() results;
+
+  public corkboardUrl;
+  public groupsUrl;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.corkboardUrl = `${environment.appEndpoint}/corkboard`;
+    this.groupsUrl = `${environment.appEndpoint}/groups/search`;
+  }
 }
