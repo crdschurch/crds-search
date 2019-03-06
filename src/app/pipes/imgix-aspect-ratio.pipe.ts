0a2bff39adc9a666ce4d886917b45bb27b843735
Luke Moderwell <luke.moderwell@gmail.com>
2019-03-06 16:10:15 -0500

    US16684: Album Art clean up

--- src/app/pipes/imgix-aspect-ratio.pipe.ts
+++ src/app/pipes/imgix-aspect-ratio.pipe.ts
@@ -3,12 +3,13 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'imgixAspectRatio'})
 export class ImgixAspectRatioPipe implements PipeTransform {
   transform(hit : any) : any {
-    let square = `?auto=format,compress&w=1&h=1&fit=crop`;
-    let rect = `?auto=format,compress&w=4&h=3&fit=crop`;
-    if (hit.contentType == 'album' || hit.contentType == 'song' || hit.contentType == 'podcast' || hit.contentType == 'episode') {
-      return hit.image + square
+    const audioList = ['album', 'song', 'podcast', 'episode'];
+    const square = `${hit.image}?auto=format,compress&w=1&h=1&fit=crop`;
+    const rect = `${hit.image}?auto=format,compress&w=4&h=3&fit=crop`;
+    if (audioList.includes(hit.contentType)) {
+      return square
     } else {
-      return hit.image + rect
+      return rect
     }
   }
 }
