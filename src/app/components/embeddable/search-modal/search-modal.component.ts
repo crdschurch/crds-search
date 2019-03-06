dc287db160715c9cc1861f034863882cf4a110ce
Luke Moderwell <luke.moderwell@gmail.com>
2019-03-06 14:02:31 -0500

    US16349: Embed Search full screen takeover

--- src/app/components/embeddable/search-modal/search-modal.component.ts
+++ src/app/components/embeddable/search-modal/search-modal.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-modal',
   templateUrl: './search-modal.component.html',
   styleUrls: ['./search-modal.component.scss']
 })
-export class SearchModalComponent implements OnInit {
+export class SearchModalComponent {
   public isVisible = false;
 
   constructor() { 
-  }
-
-  ngOnInit() {
   }
 }
