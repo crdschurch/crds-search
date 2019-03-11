2483f0fa4f24762f1d21aed6ba9eafe8295c56c4
Sarah Henney <shenney@callibrity.com>
2019-03-11 16:23:09 -0400

    Cleanup

--- cypress/integration/searchPage/search_params_in_url_spec.js
+++ cypress/integration/searchPage/search_params_in_url_spec.js
@@ -1,20 +1,20 @@
 import { SearchBar } from '../../support/SearchBar';
 
-function getUrlWithQuery(keyword, filterLabel=undefined){
+function getUrlWithQuery(keyword, filterLabel = undefined) {
   const encodedKeyword = encodeURI(keyword);
   let url = `${Cypress.config().baseUrl}/?query=${encodedKeyword}`
-  if(filterLabel !== undefined){
+  if (filterLabel !== undefined) {
     url = `${url}&menu%5BcontentType%5D=${filterLabel}`
   }
   return url;
 }
 
-describe('When someone searches:', function() {
-  beforeEach(function(){
+describe('When someone searches:', function () {
+  beforeEach(function () {
     cy.visit('/');
   })
 
-  it('For a keyword, the keyword should be included in the url', function(){
+  it('For a keyword, the keyword should be included in the url', function () {
     const keyword = 'God';
     const expectedUrl = getUrlWithQuery(keyword);
 
@@ -23,7 +23,7 @@ describe('When someone searches:', function() {
     cy.url().should('eq', expectedUrl);
   })
 
-  it('For a keyword a selects filter, the keyword and filter should be included in the url', function(){
+  it('For a keyword a selects filter, the keyword and filter should be included in the url', function () {
     const keyword = 'God';
 
     SearchBar.enterKeyword(keyword);
@@ -38,8 +38,8 @@ describe('When someone searches:', function() {
   })
 });
 
-describe('When someone navigates to a search url:', function(){
-  it('With a keyword in it, the page should load with the results for the keyword', function(){
+describe('When someone navigates to a search url:', function () {
+  it('With a keyword in it, the page should load with the results for the keyword', function () {
     const urlWithQuery = getUrlWithQuery('god');
 
     cy.visit(urlWithQuery);
@@ -48,7 +48,7 @@ describe('When someone navigates to a search url:', function(){
     cy.get('@firstResult').should('be.visible');
   })
 
-  it('With a keyword and filter in it, the page should load with the filtered results for the keyword', function(){
+  it('With a keyword and filter in it, the page should load with the filtered results for the keyword', function () {
     const filterType = 'message';
     const urlWithFilteredQuery = getUrlWithQuery('god', filterType);
 
@@ -58,7 +58,7 @@ describe('When someone navigates to a search url:', function(){
     cy.get('@firstResult').should('be.visible');
 
     cy.get('[class="ais-Menu-item ais-Menu-item--selected"]').as('selectedFilter');
-    cy.get('@selectedFilter').find('.ais-Menu-label').should('have.prop', 'textContent').then(label =>{
+    cy.get('@selectedFilter').find('.ais-Menu-label').should('have.prop', 'textContent').then(label => {
       expect(label).to.be.eq(filterType);
     })
   })
