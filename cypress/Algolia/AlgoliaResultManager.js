6a65fbb4b15c71315de5dd19156f978511a6cd10
Sarah Henney <shenney@callibrity.com>
2019-03-11 10:22:10 -0400

    Missed a sait for response

--- cypress/Algolia/AlgoliaResultManager.js
+++ cypress/Algolia/AlgoliaResultManager.js
@@ -8,7 +8,7 @@ export class AlgoliaResultManager {
   searchForKeyword(keyword){
     this._results_ready = false;
     const response = AlgoliaApi.getQueryResponse(keyword);
-    cy.wrap({response}).its('response.responseReady').then(() =>{
+    cy.wrap({response}).its('response.responseReady').should('be.true').then(() =>{
       this._hit_list = response.responseBody.results[0].hits;
       this._results_ready = true;
     })
