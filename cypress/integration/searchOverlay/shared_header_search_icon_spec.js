95a7a1a717f773c96cd4f772d95bf9e1519f9748
Sarah Henney <shenney@callibrity.com>
2019-04-19 15:37:14 -0400

    Added automation id to desktop search icon

--- cypress/integration/searchOverlay/shared_header_search_icon_spec.js
+++ cypress/integration/searchOverlay/shared_header_search_icon_spec.js
@@ -2,7 +2,7 @@ describe('lll', function () {
   it('The search overlay should open when the search icon is clicked on the homepage', function (){
     cy.visit('/');
 
-    cy.get('button[data-target="#searchModal"]').as('searchIcon').click();
+    cy.get('crds-search[id=""] > button[data-target="#searchModal"]').as('searchIcon').click();
 //TODO add id to search button to make it unique from mobile
   })
 })
