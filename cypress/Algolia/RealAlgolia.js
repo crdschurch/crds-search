2049d25b76b3b198c52e0de4ae29d6711b226833
Sarah Henney <shenney@callibrity.com>
2019-04-24 17:13:58 -0400

    test coverage for each content type in algolia

--- cypress/Algolia/RealAlgolia.js
+++ cypress/Algolia/RealAlgolia.js
@@ -4,22 +4,25 @@ import algoliasearch from "algoliasearch";
 //
 
 export class RealAlgolia {
-  static getQueryResponse(keyword) {
+  //TODO need to convert existing tests to use this
+  static fetchResultsForKeyword(keyword) {
     const responseWrapper = new ResponseWrapper();
-    // const encodedKeyword = encodeURI(keyword);
     const client = algoliasearch(Cypress.env('ALGOLIA_APP_ID'), Cypress.env('ALGOLIA_API_KEY'));
     const index = client.initIndex(Cypress.env('ALGOLIA_INDEX'));
     index.search({ query: keyword }).then(rsp => {
       responseWrapper.responseBody = rsp;
     });
+    return responseWrapper;
+  }
 
-    // cy.request({
-    //   method: 'POST',
-    //   url: `https://${Cypress.env('ALGOLIA_APP_ID')}-dsn.algolia.net/1/indexes/*/queries?x-algolia-application-id=${Cypress.env('ALGOLIA_APP_ID')}&x-algolia-api-key=${Cypress.env('ALGOLIA_API_KEY')}`,
-    //   body: { "requests": [{ "indexName": Cypress.env('ALGOLIA_INDEX'), "params": `query=${encodedKeyword}` }] }
-    // }).then(response => {
-    //   responseWrapper.responseBody = response.body;
-    // });
+  static fetchResultsByContentType(contentType) {
+    const responseWrapper = new ResponseWrapper();
+
+    const client = algoliasearch(Cypress.env('ALGOLIA_APP_ID'), Cypress.env('ALGOLIA_API_KEY'));
+    const index = client.initIndex(Cypress.env('ALGOLIA_INDEX'));
+    index.search({ facetFilters: [`contentType:${contentType}`] }).then(rsp => {
+      responseWrapper.responseBody = rsp;
+    });
     return responseWrapper;
   }
 }
--- cypress/integration/APIOnly/response_structure_spec.js
+++ cypress/integration/APIOnly/response_structure_spec.js
@@ -1,33 +1,64 @@
-import algoliasearch from 'algoliasearch';
-import { AlgoliaApi } from '../../Algolia/AlgoliaApi';
 import { RealAlgolia } from '../../Algolia/RealAlgolia';
 
 /**
  * Verifies that the Algolia responses contain what we expect so we know our stubbed responses are accurate.
  */
+//TODO convert old Algolia query manager to use this
+describe('Tests the response for a keyword contain the expected properties', function () {
+  const keywords = ['god', 'lksjd;flakjds'];
+  keywords.forEach(keyword => {
+    it(`Response for keyword ${keyword} should have keyword, hit count, and pagination information`, function () {
+      const responseWrapper = RealAlgolia.fetchResultsForKeyword(keyword);
+      cy.wrap({ responseWrapper }).its('responseWrapper.responseReady').should('be.true').then(() => {
+        return responseWrapper.responseBody;
+      }).then(response => {
+        expect(response).to.have.property('query', keyword);
+        expect(response).to.have.property('params');
 
-describe('Tests the responses from Algolia contain what we expect', function () {
-  const keyword = 'god'; //TODO use different keywords -
-  let response;
-  before(function () {
-    const responseWrapper = RealAlgolia.getQueryResponse(keyword);
-    return cy.wrap({responseWrapper}).its('responseWrapper.responseReady').should('be.true').then(() =>{
-      response = responseWrapper.responseBody;
+        expect(response).to.have.property('hitsPerPage');
+        expect(response).to.have.property('hits').with.property('length').lte(response.hitsPerPage);
+
+        expect(response).to.have.property('nbHits');
+        expect(response).to.have.property('nbPages', Math.ceil(response.nbHits / response.hitsPerPage));
+      });
     });
   });
-
-  it('Response should have keyword, hit count, and pagination information', function () {
-    expect(response).to.have.property('query', keyword);
-    expect(response).to.have.property('params');
-
-    expect(response).to.have.property('hitsPerPage');
-    expect(response).to.have.property('hits').with.property('length').lte(response.hitsPerPage);
-
-    expect(response).to.have.property('nbHits');
-    expect(response).to.have.property('nbPages', Math.ceil(response.nbHits / response.hitsPerPage));
-  });
-
-  it('Hits should have...', function () {
-    //TODO what should this have?
-  })
 });
+
+const standardProperties = ['title', 'category', 'tags', 'description', 'url', 'objectID', 'image']
+const contentTypeProperties = {
+  page: [],
+  message: ['date', 'duration', 'date_timestamp', 'series'],
+  series: ['start_date', 'end_date', 'messages', 'date_timestamp'],
+  video: ['date', 'duration', 'date_timestamp'],
+  article: ['date', 'duration', 'date_timestamp', 'author'],
+  episode: ['date', 'duration', 'date_timestamp', 'podcast'],
+  song: ['date', 'duration', 'date_timestamp'],
+  author: [],
+  promo: ['date', 'date_timestamp'],
+  location: ['serviceTimes', 'map_url'],
+  podcast: ['author', 'children_count'],
+  category: [],
+  album: ['date', 'duration', 'date_timestamp', 'author'],
+}
+
+describe('Tests the responses for each content type have the expected properties', function () {
+  Object.keys(contentTypeProperties).forEach(type => {
+    it(`Testing ${type} properties`, function () {
+      const responseWrapper = RealAlgolia.fetchResultsByContentType(type);
+      cy.wrap({ responseWrapper }).its('responseWrapper.responseReady').should('be.true').then(() => {
+        const response = responseWrapper.responseBody;
+
+        expect(response).to.have.property('hits').with.property('length').gte('0');
+        return response.hits[0];
+      }).then(firstHit => {
+        expect(firstHit).to.have.property('contentType', type);
+
+        const expectedProperties = standardProperties.concat(contentTypeProperties[type]);
+        expectedProperties.forEach(prop => {
+          expect(firstHit).to.have.property(prop).and.to.not.be.undefined;
+        })
+      })
+    })
+  })
+})
