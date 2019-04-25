4810946f88014552ec2fca5ec91f6a9397472b7c
Sarah Henney <shenney@callibrity.com>
2019-04-25 12:21:25 -0400

    Cleanup

--- cypress/Algolia/AlgoliaAPI.js
+++ cypress/Algolia/AlgoliaAPI.js
@@ -1,7 +1,5 @@
 import algoliasearch from "algoliasearch";
 
-//
-
 export class AlgoliaAPI {
   static searchByKeyword(keyword) {
     return AlgoliaAPI.search({query: keyword});
@@ -16,26 +14,4 @@ export class AlgoliaAPI {
     const index = client.initIndex(Cypress.env('ALGOLIA_INDEX'));
     return index.search(queryObject);
   }
-
-  // //TODO need to convert existing tests to use this
-  // static fetchResultsForKeyword(keyword) {
-  //   const responseWrapper = new ResponseWrapper();
-  //   const client = algoliasearch(Cypress.env('ALGOLIA_APP_ID'), Cypress.env('ALGOLIA_API_KEY'));
-  //   const index = client.initIndex(Cypress.env('ALGOLIA_INDEX'));
-  //   index.search({ query: keyword }).then(rsp => {
-  //     responseWrapper.responseBody = rsp;
-  //   });
-  //   return responseWrapper;
-  // }
-
-  // static fetchResultsByContentType(contentType) {
-  //   const responseWrapper = new ResponseWrapper();
-
-  //   const client = algoliasearch(Cypress.env('ALGOLIA_APP_ID'), Cypress.env('ALGOLIA_API_KEY'));
-  //   const index = client.initIndex(Cypress.env('ALGOLIA_INDEX'));
-  //   index.search({ facetFilters: [`contentType:${contentType}`] }).then(rsp => {
-  //     responseWrapper.responseBody = rsp;
-  //   });
-  //   return responseWrapper;
-  // }
 }
