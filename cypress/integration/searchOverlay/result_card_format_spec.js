991bf863d337ca29b3f6cdf02944ad398c34c27a
Sarah Henney <shenney@callibrity.com>
2019-04-30 16:50:13 -0400

    Slightly more efficient search result card checker

--- cypress/integration/searchOverlay/result_card_format_spec.js
+++ cypress/integration/searchOverlay/result_card_format_spec.js
@@ -1,9 +1,123 @@
-import { SearchPanelFactory, ResultCard, TheFilter } from "../../support/SearchPanel";
+import { SearchPanelFactory, ResultCard } from "../../support/SearchPanel";
 
 //Tests the results are formatted by content
+const resultContentSpecs = [
+  {
+    keyword: 'God',
+    filter: 'page',
+    cardComponents: ['title', 'description', 'hitUrl']
+  },
+  {
+    keyword: 'God',
+    filter: 'message',
+    cardComponents: ['title', 'description', 'image', 'imageTimestampOverlay', 'date', 'series']
+  },
+  {
+    keyword: 'God',
+    filter: 'series',
+    cardComponents: ['title', 'description', 'image', 'seriesDate']
+  },
+  {
+    keyword: 'God',
+    filter: 'video',
+    cardComponents: ['title', 'description', 'image', 'imageTimestampOverlay', 'date', 'category']
+  },
+  {
+    keyword: 'God',
+    filter: 'article',
+    cardComponents: ['title', 'description', 'image', 'imageTimestampOverlay', 'date', 'category', 'author']
+  },
+  {
+    keyword: 'God',
+    filter: 'episode',
+    cardComponents: ['title', 'description', 'image', 'imageTimestampOverlay', 'date', 'category']
+  },
+  {
+    keyword: 'God',
+    filter: 'song',
+    cardComponents: ['title', 'description', 'image', 'date']
+  },
+  {
+    keyword: 'God',
+    filter: 'promo',
+    cardComponents: ['title', 'description', 'image', 'date', 'hitUrl']
+  },
+  {
+    keyword: 'God',
+    filter: 'author',
+    cardComponents: ['title', 'description', 'image']
+  },
+  {
+    keyword: 'Wherever',
+    filter: 'album',
+    cardComponents: ['title', 'description', 'image', 'date']
+  },
+  {
+    keyword: 'Oakley',
+    filter: 'location',
+    cardComponents: ['title', 'description', 'image', 'serviceTimes', 'directionsLink']
+  },
+  {
+    keyword: 'IKR',
+    filter: 'podcast',
+    cardComponents: ['title', 'description', 'image']
+  }
+]
+
+function verifyCardHasComponent(resultCard, component) {
+  switch (component) {
+    case 'title':
+      resultCard.title.should('be.visible').and('have.prop', 'href');
+      break;
+    case 'description':
+      resultCard.description.should('be.visible');
+      break;
+    case 'hitUrl':
+      resultCard.hitUrl.should('be.visible');
+      break;
+    case 'image':
+      resultCard.image.should('be.visible');
+      break;
+    case 'imageTimestampOverlay':
+      resultCard.imageTimestampOverlay.should('be.visible');
+      break;
+    case 'date':
+      resultCard.date.should('be.visible');
+      break;
+    case 'series':
+      resultCard.series.should('be.visible');
+      break;
+    case 'seriesDate':
+      resultCard.seriesDate.should('be.visible');
+      break;
+    case 'category':
+      resultCard.category.should('be.visible');
+      break;
+    case 'author':
+      resultCard.author.should('be.visible');
+      break;
+    case 'serviceTimes':
+      resultCard.serviceTimes.should('be.visible');
+      break;
+    case 'directionsLink':
+      resultCard.directionsLink.should('be.visible');
+      break;
+    default:
+      throw new Error(`Display validation for content "${component}" does not exist yet.`);
+  }
+}
+
+// function reloadAndSearchIfDifferentKeyword(currentKeyword, newKeyword) {
+//   if(currentKeyword !== newKeyword){
+//     cy.visit('http://localhost:3000/');
+//     search.clearedSearchField.type(type.keyword);
+//     currentKeyword = type.keyword;
+//   }
+// }
 
 describe('Search results can be filtered by type and cards are formatted correctly', function () {
   let search;
+  let currentKeyword;
   before(function () {
     // cy.visit('/firstimpressions');
 
@@ -13,188 +127,29 @@ describe('Search results can be filtered by type and cards are formatted correct
 
     //TODO build for /search so can serve locally
     cy.visit('http://localhost:3000/');///search');
-    search = SearchPanelFactory.SearchPage();
-    search.clearedSearchField.type('God');
-    //search.showMoreFilters.click();
   })
 
   beforeEach(function () {
-    SearchPanelFactory.SearchPage();
-  })
-
-  it('Tests Page filter and card layout', function () {
-    search.getFilterByName('page').click();
-    search.selectedFilterLabel.should('have.text', 'page');
-    search.resultList.first().as('firstPage');
-
-    const firstCard = new ResultCard('firstPage');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.hitUrl.should('be.visible');
-  })
-
-  it('Tests Message filter and card layout', function () {
-    search.getFilterByName('message').click();
-    search.selectedFilterLabel.should('have.text', 'message');
-    search.resultList.first().as('firstMessage');
-
-    const firstCard = new ResultCard('firstMessage');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.series.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.imageTimestampOverlay.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Series filter and card layout', function () {
-    search.getFilterByName('series').click();
-    search.selectedFilterLabel.should('have.text', 'series');
-    search.resultList.first().as('firstSeries');
-
-    const firstCard = new ResultCard('firstSeries');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.seriesDate.should('be.visible');
-  })
-
-  it('Tests Video filter and card layout', function () {
-    search.getFilterByName('video').click();
-    search.selectedFilterLabel.should('have.text', 'video');
-    search.resultList.first().as('firstVideo');
-
-    const firstCard = new ResultCard('firstVideo');
-    firstCard.category.should('be.visible');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.imageTimestampOverlay.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Article filter and card layout', function () {
-    search.getFilterByName('article').click();
-    search.selectedFilterLabel.should('have.text', 'article');
-    search.resultList.first().as('firstArticle');
-
-    const firstCard = new ResultCard('firstArticle');
-    firstCard.category.should('be.visible');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.author.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.imageTimestampOverlay.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Episode filter and card layout', function () {
-    search.getFilterByName('episode').click();
-    search.selectedFilterLabel.should('have.text', 'episode');
-    search.resultList.first().as('firstEpisode');
-
-    const firstCard = new ResultCard('firstEpisode');
-    firstCard.category.should('be.visible');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.imageTimestampOverlay.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Song filter and card layout', function () {
-    search.getFilterByName('song').click();
-    search.selectedFilterLabel.should('have.text', 'song');
-    search.resultList.first().as('firstSong');
-
-    const firstCard = new ResultCard('firstSong');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Promo filter and card layout', function () {
-    search.getFilterByName('promo').click();
-    search.selectedFilterLabel.should('have.text', 'promo');
-    search.resultList.first().as('firstPromo');
-
-    const firstCard = new ResultCard('firstPromo');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.date.should('be.visible');
-    firstCard.hitUrl.should('be.visible');
-  })
-
-  it('Tests Author filter and card layout', function () {
-    search.getFilterByName('author').click();
-    search.selectedFilterLabel.should('have.text', 'author');
-    search.resultList.first().as('firstAuthor');
-
-    const firstCard = new ResultCard('firstAuthor');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-  })
-})
-
-//podcast, category
-describe('Search results can be filtered by type and cards are formatted correctly (smaller categories)', function () {
-  let search;
-  beforeEach(function () {
-    // cy.visit('/firstimpressions');
-
-    // //DE6720 - force open the modal
-    // cy.get('button[data-target="#searchModal"]').first().click({ force: true });
-    // search = SearchPanelFactory.MobileSharedHeaderSearchModal();
-
-    //TODO build for /search so can serve locally
-    cy.visit('http://localhost:3000/');///search');
     search = SearchPanelFactory.SearchPage();
+    // search.clearedSearchField.type('God');
   })
 
-  it('Tests Album filter and card layout', function () {
-    search.clearedSearchField.type('Whatever');
-    search.showMoreFilters.click();
+  resultContentSpecs.forEach(type => {
+    it(`Tests ${type.filter} filter and card layout`, function () {
+      if(currentKeyword !== type.keyword){
+        cy.visit('http://localhost:3000/');
+        search.clearedSearchField.type(type.keyword);
+        currentKeyword = type.keyword;
+      }
 
-    search.getFilterByName('album').click();
-    search.selectedFilterLabel.should('have.text', 'album');
-    search.resultList.first().as('firstAlbum');
+      search.getFilterByName(type.filter).click(); //TODO need to wait for filter to apply before any further assertions
+      search.selectedFilterLabel.should('have.text', type.filter);
+      search.resultList.first().as(`first${type.filter}Card`);
 
-    const firstCard = new ResultCard('firstAlbum');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.date.should('be.visible');
-  })
-
-  it('Tests Location filter and card layout', function () {
-    search.clearedSearchField.type('Oakley');
-    search.showMoreFilters.click();
-
-    search.getFilterByName('location').click();
-    search.selectedFilterLabel.should('have.text', 'location');
-    search.resultList.first().as('firstLocation');
-
-    const firstCard = new ResultCard('firstLocation');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
-    firstCard.serviceTimes.should('be.visible');
-    firstCard.directionsLink.should('be.visible').and('have.prop', 'href');
-  })
-
-  it('Tests Podcast filter and card layout', function () {
-    search.clearedSearchField.type('IKR');
-
-    search.getFilterByName('podcast').click();
-    search.selectedFilterLabel.should('have.text', 'podcast');
-    search.resultList.first().as('firstPodcast');
-
-    const firstCard = new ResultCard('firstPodcast');
-    firstCard.title.should('be.visible').and('have.prop', 'href');
-    firstCard.description.should('be.visible');
-    firstCard.image.should('be.visible');
+      const firstCard = new ResultCard(`first${type.filter}Card`);
+      type.cardComponents.forEach(content => {
+        verifyCardHasComponent(firstCard, content);
+      })
+    })
   })
 })
