43e289f3318e09cd9731c5e7066fab13c9b117bb
Lynch <llynch@callibrity.com>
2020-04-27 17:07:11 -0400

    update searchh cypress test

--- cypress/integration/searchOverlayCompatible/result_navigation_spec.js
+++ cypress/integration/searchOverlayCompatible/result_navigation_spec.js
@@ -1,10 +1,14 @@
-import { SearchPanelFactory } from '../../SearchPanel/SearchPanel'
+import { SearchPanelFactory } from '../../SearchPanel/SearchPanel';
+
+ const errorsToIgnore = [/.*Cannot set property\W+\w+\W+of undefined.*/, /.*Script error.*/]; 
 
 describe('Tests the expected page opens when result is clicked', () => {
   let search;
   beforeEach(() => {
-    cy.visit('/search');
-    search = SearchPanelFactory.SearchPage();
+  const errorsToIgnore = [/.*Cannot set property\W+\w+\W+of undefined.*/];  
+  cy.ignoreMatchingErrors(errorsToIgnore);
+  cy.visit('/search');
+  search = SearchPanelFactory.SearchPage();
   });
 
   //Use below for testing the search overlay
@@ -25,20 +29,21 @@ describe('Tests the expected page opens when result is clicked', () => {
   })
 
   it('checks result requiring authentication opens /signin', () => {
+    const errorsToIgnore = [/.*Script error.*/];  
+    cy.ignoreMatchingErrors(errorsToIgnore);
     const requiresAuthUrl = `${Cypress.env('CRDS_ENDPOINT')}/profile/personal`;
 
     search.clearedSearchField.type('Profile').then(() => {
-      search.results.findByHref(requiresAuthUrl).click();
-      cy.contains('Sign In').should('exist').and('be.visible');
-      cy.url().should('match', new RegExp(`${Cypress.env('CRDS_ENDPOINT')}/signin/?`));
+    search.results.findByHref(requiresAuthUrl).click();
+    cy.contains('Sign In').should('exist').and('be.visible');
+    cy.url().should('match', new RegExp(`${Cypress.env('CRDS_ENDPOINT')}/signin/?`));
     });
   })
 
   it('checks result for page from Contentful opens that page', () => {
     const crdsNetUrl = `${Cypress.env('CRDS_ENDPOINT')}/wayfinder-leader-resources/`;
-
     search.clearedSearchField.type('wayfinder').then(() => {
-      search.results.findByHref(crdsNetUrl).click();
+      search.results.findByHref(crdsNetUrl, { delay: 2000 }).click();
       cy.url().should('match', new RegExp(`${crdsNetUrl}/?`));
     });
   })
