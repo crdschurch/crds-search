b0851121eeda0971c3741f9d2d1e311fc35141a4
janssejl <ll>
2020-12-02 13:05:49 -0500

    pluralizing filter for popular now

--- src/app/components/hits/hits.component.ts
+++ src/app/components/hits/hits.component.ts
@@ -82,4 +82,10 @@ export class HitsComponent implements OnChanges, OnInit, OnDestroy {
       return el;
     }
   }
+  public formatLabel(label) {
+    if(!label) return;
+    label = label.replace("_", " ");
+    if (label.endsWith("s") || label == "all") return label;
+    return label + "s";
+  }
 }
